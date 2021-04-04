d22ddf2605b2a719b79c859a4b842b4c76084665
Peter Hoang <peter.w.hoang@gmail.com>
2021-04-04 19:51:27 -0400

    Add DB connection

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+connectDB();
+
 app.use(express.json({ strict: false }));
 app.get('/', (req, res) => res.send('API Running...'));
+
+// Define routes
+app.use('/api/books', require('./routes/api/books'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
